b4b571dc4115c29fe7da882d76535b22d392e76a
EduardIskhakov <edik.ru222@gmail.com>
2026-06-29 22:57:00 +0300

    l2.14

---
+++ L2.14/cmd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	orDone := make(chan interface{})
+
+	if len(channels) == 0 {
+		close(orDone)
+		return orDone
+	}
+
+	var once sync.Once
+
+	for _, ch := range channels {
+		go func(ch <-chan interface{}) {
+			select {
+			case <-ch:
+				once.Do(func() {
+					close(orDone)
+				})
+			case <-orDone:
+			}
+		}(ch)
+	}
+	return orDone
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
